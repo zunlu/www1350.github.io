0f5ab709499a89700c082f9828c94fef307b5c79
Absurd <www_1350@163.com>
2016-04-13 14:25:04 +0800

    换一个目录

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name       : 'Absurd Blog',         // 博客名称
     owner           : 'www1350',           // github 用户名
-    repo            : 'www1350.github.io',// github 中对应项目名
+    repo            : 'javaweb',// github 中对应项目名
     duoshuo_id      : 'absurd',            // 在第三方评论插件多说申请站点的子域名
     // access_token : '',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '15'                    // 默认一页显示几篇文章
