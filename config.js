8f32f3ad04a42b76aa371cbc9fe50f287eade0e7
www1350 <www_1350@163.com>
2016-04-18 11:03:03 +0800

    https://github.com/settings/tokens

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
     owner           : 'www1350',           // github 用户名
     repo            : 'javaweb',// github 中对应项目名
     duoshuo_id      : 'absurd',            // 在第三方评论插件多说申请站点的子域名
-    // access_token : '',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    access_token : '2c0f80b6118e370986d566a5085dcce3d430b0c3',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '15'                    // 默认一页显示几篇文章
 }
 
