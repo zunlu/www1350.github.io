8335faefd54bac6d294128151df341c94051a7a1
www1350 <www_1350@163.com>
2016-04-18 11:18:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _config = {
     owner           : 'www1350',           // github 用户名
     repo            : 'javaweb',// github 中对应项目名
     duoshuo_id      : 'absurd',            // 在第三方评论插件多说申请站点的子域名
-    access_token : 'f161532b1c1f9a69070b2f88db7d8f2d7f39a7a3',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    access_token : '0eb05f750d47fbb4eb511b59b18cd1fa06ce263a',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     per_page        : '15'                    // 默认一页显示几篇文章
 }
 
